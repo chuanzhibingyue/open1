aaa7c8b1aa7713e554f87a25d8bf8e82679a572a
DouWei <vincent@DOU-MAC-MINI.local>
2017-02-17 23:47:37 +0800

    update scripts

--- start-dubbo.sh
+++ start-dubbo.sh
@@ -3,11 +3,11 @@
 echo start simple registry
 HOME=`pwd`
 cd $HOME/env-dubbo/dubbo-registry-simple/
-./bin/restart.sh
+./bin/start.sh
 echo done
 
 echo start simple monitor
 cd $HOME/env-dubbo/dubbo-monitor-simple/
-./bin/restart.sh
+./bin/start.sh
 echo done.  http://127.0.0.1:8080/
 
--- stop-services.sh
+++ stop-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo kill services 
-kill -9 `ps -ef|grep 'pay-service-'|grep -v grep|awk '{print $2}'`
+kill -9 `ps -ef|grep 'pay-'|grep -v grep|awk '{print $2}'`
 
 echo done.  
 
